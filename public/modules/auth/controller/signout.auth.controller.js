1f7ca754ddf7ff42f912943dcf217f777f1bacf9
munzir <munzir.suliman@outlook.com>
2014-12-26 05:08:56 +0400

    fix bug in logout
    fix bug in logout

--- public/modules/auth/controller/signout.auth.controller.js
+++ public/modules/auth/controller/signout.auth.controller.js
@@ -1,9 +1,9 @@
 'user strict';
 
-angular.module('authModule').controller('signoutAuthController', ['registerUserConfigFactory', '$http', '$location', function (registerUserConfigFactory, $http, $location) {
+angular.module('authModule').controller('signoutAuthController', ['registerUserConfigFactory', '$http', '$state', function (registerUserConfigFactory, $http, $state) {
 	$http.get('/logout')
 	.success(function (data, success) {
 		registerUserConfigFactory.clearUserInfo();
-		$location.path('/home');
+		$state.go('home', {}, {reload: true});
 	});
 }]);

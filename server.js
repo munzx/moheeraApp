2b86cd933893df63e7e7af3084a847d7ef928577
munzir <munzir.suliman@outlook.com>
2015-01-03 03:29:28 +0400

    change app port
    change app port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var express = require('express'),
-	app = express();
+	app = express(),
+	port = process.env.PORT || 3000;
 
 // Initilize with config file
 require('./app/config/init')(app, express);
@@ -9,6 +10,6 @@ require('./app/config/init')(app, express);
 //initilize routes
 require('./app/config/routes')(app);
 
-app.listen(3000, function () {
+app.listen(port, function () {
 	console.log('Bism Allah , Server runs on port 3000');
 });

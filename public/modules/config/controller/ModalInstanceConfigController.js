2c63075d4c7090c3a57ce1f04b320b27406762bd
munzir <munzir.suliman@outlook.com>
2015-01-08 12:57:59 +0400

    test
    test

--- public/modules/config/controller/ModalInstanceConfigController.js
+++ public/modules/config/controller/ModalInstanceConfigController.js
@@ -3,16 +3,16 @@
 angular.module('moheera').controller('ModalInstanceConfigController', ['$scope', '$rootScope', '$modalInstance', function ($scope, $rootScope, $modalInstance) {
 	// switch(service){
 	// 	case 'cart':
-	// 	$scope.msg = 'to add this product to cart';
+	// 	$scope.msg= 'to add this product to cart';
 	// 	break;
 	// 	case 'heart':
-	// 	$scope.msg = 'to heart this product!';
+	// 	$scope.msg= 'to heart this product!';
 	// 	break;
 	// 	case 'comment':
-	// 	$scope.msg = 'to add a comment';
+	// 	$scope.msg= 'to add a comment';
 	// 	break;
 	// 	default:
-	// 	$scope.msg = 'to complete this operation';
+	// 	$scope.msg= 'to complete this operation';
 	// }
 	$scope.cancel = function () {
 		$modalInstance.dismiss('cancel');

b72ae654570a5e11a5349d1e6cd60082918c8012
munzir <munzir.suliman@outlook.com>
2015-01-20 02:04:31 +0400

    test
    test

--- app/config/email/config.email.js
+++ app/config/email/config.email.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var mandrillConnect = require('mandrill-api/mandrill'),
-_ = require('lodash');
+var mandrillConnect = require('mandrill-api/mandrill.js'),
+	_ = require('lodash');
 
 module.exports.sendEmail = function (recipients) {
 	if(recipients){
@@ -12,58 +12,57 @@ module.exports.sendEmail = function (recipients) {
 
 		//emails sender
 		var sender = function (data) {
-			if(mandrillConnect){
-				mandrillConnect = new mandrillConnect.Mandrill('GaPcAziEdCK77u1876Sb8g');
-				var info = {
-					template_name: "moheeraOrder",
-					name: "Moheera Team",
-					subject: 'New Order',
-					to: data
-				}
+			mandrillConnect = new mandrillConnect.Mandrill('GaPcAziEdCK77u1876Sb8g');
+			var info = {
+				template_name: "moheeraOrder",
+				name: "Moheera Team",
+				subject: 'New Order',
+				to: data
+			}
 
-				var message = {
-					"html": "<p>Example HTML content</p>",
-					"text": "Example text content",
-					"subject": info.subject,
-					"from_email": "info@moheera.com",
-					"from_name": info.name,
-					"to": info.to,
-					"headers": {
-						"Reply-To": "message.reply@example.com"
-					},
-					"important": false,
-					"track_opens": null,
-					"track_clicks": null,
-					"auto_text": null,
-					"auto_html": null,
-					"inline_css": null,
-					"url_strip_qs": null,
-					"preserve_recipients": null,
-					"view_content_link": null,
-					"bcc_address": null,
-					"tracking_domain": null,
-					"signing_domain": null,
-					"return_path_domain": null,
-					"merge": true,
-					"merge_language": "mailchimp",
-					"global_merge_vars": [],
-					"merge_vars": [],
-					"tags": ['test'],
-					"subaccount": null,
-					"google_analytics_domains": [],
-					"google_analytics_campaign": null,
-					"metadata": null,
-					"recipient_metadata": [],
-					"attachments": [],
-					"images": []
-				};
-				var async = false;
-				var ip_pool = null;
-				var send_at = null;
-				mandrillConnect.messages.sendTemplate({"template_name": info.template_name, "template_content": null, "message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function(result) {
-					console.log('Email sent');
-			    /*
-			    [{
+			var message = {
+				"html": "<p>Example HTML content</p>",
+				"text": "Example text content",
+				"subject": info.subject,
+				"from_email": "info@moheera.com",
+				"from_name": info.name,
+				"to": info.to,
+				"headers": {
+					"Reply-To": "message.reply@example.com"
+				},
+				"important": false,
+				"track_opens": null,
+				"track_clicks": null,
+				"auto_text": null,
+				"auto_html": null,
+				"inline_css": null,
+				"url_strip_qs": null,
+				"preserve_recipients": null,
+				"view_content_link": null,
+				"bcc_address": null,
+				"tracking_domain": null,
+				"signing_domain": null,
+				"return_path_domain": null,
+				"merge": true,
+				"merge_language": "mailchimp",
+				"global_merge_vars": [],
+				"merge_vars": [],
+				"tags": ['test'],
+				"subaccount": null,
+				"google_analytics_domains": [],
+				"google_analytics_campaign": null,
+				"metadata": null,
+				"recipient_metadata": [],
+				"attachments": [],
+				"images": []
+			};
+			var async = false;
+			var ip_pool = null;
+			var send_at = null;
+			mandrillConnect.messages.sendTemplate({"template_name": info.template_name, "template_content": null, "message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function(result) {
+				console.log('Email sent');
+		    /*
+		    [{
 		            "email": "recipient.email@example.com",
 		            "status": "sent",
 		            "reject_reason": "hard-bounce",
@@ -75,7 +74,6 @@ module.exports.sendEmail = function (recipients) {
 			    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
 			    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
 			});
-			}
 		}
 
 		//loop through recipients to get each data create object with users data

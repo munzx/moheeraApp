b0c646a7af184c02cbb0c426f09d59778e9945f3
munzir <munzir.suliman@outlook.com>
2015-01-21 21:20:01 +0400

    add image preview
    add image preview

--- public/modules/config/directive/watch.image.config.directive.js
+++ public/modules/config/directive/watch.image.config.directive.js
@@ -5,9 +5,21 @@ angular.module('moheera').directive('watchImageConfigDirective', [function () {
 		restrict: 'A',
 		replace: false,
 		transclude: false,
+		scope: {
+			"id": "@id"
+		},
 		link: function (scope, elem, attrs) {
-			elem.bind('change', function () {
-				console.log(document.getElementById('image1').value);
+			document.getElementById(scope.id + 'Preview').onclick = function () {
+				document.getElementById(scope.id).click();
+			}
+			elem.bind('change', function (e) {
+            var reader = new FileReader();
+            
+            reader.onload = function (image) {
+            	var path = image.target.result;
+            	document.getElementById(scope.id + 'Preview').setAttribute('src', path);
+            }
+            reader.readAsDataURL(elem[0].files[0]);
 			});
 		}
 	}

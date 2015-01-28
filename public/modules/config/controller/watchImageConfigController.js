476713136a8f77db80721884245eb50977f95116
munzir <munzir.suliman@outlook.com>
2015-01-28 08:08:20 +0400

    fix sidebar link
    fix sidebar link

--- public/modules/config/controller/watchImageConfigController.js
+++
@@ -1,9 +0,0 @@
-'user strict';
-
-angular.module('moheera').controller('watchImageConfigController', ['$scope', '$rootScope', '$modalInstance', function ($scope, $rootScope, $modalInstance, test) {
-	$scope.cancel = function () {
-		$modalInstance.dismiss('cancel');
-	}
-
-	console.log($scope.newProduct.image1);
-}])
